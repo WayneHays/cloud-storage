1094e00c1d710a4dda2bc37eeb089214e3ed83ca
WayneHays <waynehays1605@gmail.com>
2026-02-10 16:43:34 +0300

    feat(dto): add RegisterRequest dto - username 3-50 chars

---
+++ src/main/java/com/waynehays/cloudfilestorage/dto/RegisterRequest.java
@@ -0,0 +1,17 @@
+package com.waynehays.cloudfilestorage.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+
+        @NotBlank(message = "Username must not be empty")
+        @Pattern(regexp = "^\\w{3,50}$", message = "Username must be 3-50 characters, letters, digits, underscore only")
+        String username,
+
+        @NotBlank(message = "Password cannot be empty")
+        @Size(min = 6, max = 128, message = "Password must be at least 6 and max 128 symbols")
+        String password
+) {
+}
