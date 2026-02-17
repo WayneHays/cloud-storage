c6b82cc1cf60e3ac3b3e7781c5c0682aa5497d06
WayneHays <waynehays1605@gmail.com>
2026-02-17 17:32:18 +0300

    feat: add sign in logic

--- src/main/java/com/waynehays/cloudfilestorage/controller/AuthController.java
+++ src/main/java/com/waynehays/cloudfilestorage/controller/AuthController.java
@@ -1,13 +1,23 @@
 package com.waynehays.cloudfilestorage.controller;
 
-import com.waynehays.cloudfilestorage.dto.request.RegistrationDto;
+import com.waynehays.cloudfilestorage.dto.request.SignInRequest;
+import com.waynehays.cloudfilestorage.dto.request.SignUpRequest;
 import com.waynehays.cloudfilestorage.dto.response.UserDto;
 import com.waynehays.cloudfilestorage.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +29,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final SecurityContextRepository securityContextRepository;
+    private final SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder.getContextHolderStrategy();
 
     @PostMapping("/sign-up")
-    public ResponseEntity<UserDto> register(@RequestBody @Valid RegistrationDto dto) {
-        return new ResponseEntity<>(userService.register(dto), HttpStatus.CREATED);
+    public ResponseEntity<UserDto> signUp(@RequestBody @Valid SignUpRequest dto,
+                                          HttpServletRequest request,
+                                          HttpServletResponse response) {
+        UserDto registeredUser = userService.signUp(dto);
+        processLogin(dto.username(), dto.password(), request, response);
+
+        return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<UserDto> signIn(@RequestBody @Valid SignInRequest dto,
+                                          HttpServletRequest request,
+                                          HttpServletResponse response) {
+        Authentication authentication = processLogin(dto.username(), dto.password(), request, response);
+
+        UserDto userDto = new UserDto(authentication.getName());
+        return ResponseEntity.ok(userDto);
+    }
+
+    private Authentication processLogin(String username, String password, HttpServletRequest request, HttpServletResponse response) {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+        Authentication authentication = authenticationManager.authenticate(token);
+
+        SecurityContext context = securityContextHolderStrategy.createEmptyContext();
+        context.setAuthentication(authentication);
+        securityContextHolderStrategy.setContext(context);
+        securityContextRepository.saveContext(context, request, response);
+        return authentication;
     }
 }
