c5939425db5a3547104785e03f130f7dfceb55f6
WayneHays <waynehays1605@gmail.com>
2026-02-17 17:33:05 +0300

    feat: add controller for user (api/user/me)

---
+++ src/main/java/com/waynehays/cloudfilestorage/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.waynehays.cloudfilestorage.controller;
+
+import com.waynehays.cloudfilestorage.dto.response.UserDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me(@AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity.ok(new UserDto(userDetails.getUsername()));
+    }
+}
