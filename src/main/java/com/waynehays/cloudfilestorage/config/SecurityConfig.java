17b073486a4e3b6e5c2463db7b26a5a8a4d70dfb
WayneHays <waynehays1605@gmail.com>
2026-02-18 10:44:44 +0300

    chore: SecurityConfig -> remove import for @RequiredArgsConstructor

--- src/main/java/com/waynehays/cloudfilestorage/config/SecurityConfig.java
+++ src/main/java/com/waynehays/cloudfilestorage/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.waynehays.cloudfilestorage.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
