0bef65d61cc5d1af576d1896e2ad1b053c3169a2
WayneHays <waynehays1605@gmail.com>
2026-02-18 10:43:18 +0300

    test: add tests for UserController (/me)

---
+++ src/test/java/com/waynehays/cloudfilestorage/controller/UserControllerTest.java
@@ -0,0 +1,41 @@
+package com.waynehays.cloudfilestorage.controller;
+
+import com.waynehays.cloudfilestorage.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+@Import(SecurityConfig.class)
+class UserControllerTest {
+    private static final String PATH_ME = "/api/user/me";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Should return 200 with authorised user")
+    @WithMockUser(username = "name")
+    void shouldReturn200_ifAuthorisedUser() throws Exception {
+        mockMvc.perform(get(PATH_ME))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("name"));
+    }
+
+    @Test
+    @DisplayName("Should return 401 with anonymous user")
+    @WithAnonymousUser
+    void shouldReturn401_ifAnonymousUser() throws Exception {
+        mockMvc.perform(get(PATH_ME))
+                .andExpect(status().isUnauthorized());
+    }
+}
