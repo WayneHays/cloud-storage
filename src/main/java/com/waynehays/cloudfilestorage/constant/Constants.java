99acce6db418d9a9eeae0fd96b1e97cb6b0656a4
WayneHays <waynehays1605@gmail.com>
2026-02-20 14:49:56 +0300

    feat(constants): add Constants class for repeated constants

---
+++ src/main/java/com/waynehays/cloudfilestorage/constant/Constants.java
@@ -0,0 +1,10 @@
+package com.waynehays.cloudfilestorage.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Constants {
+    public static final String PATH_SEPARATOR = "/";
+    public static final String EXTENSION_SEPARATOR = ".";
+}
