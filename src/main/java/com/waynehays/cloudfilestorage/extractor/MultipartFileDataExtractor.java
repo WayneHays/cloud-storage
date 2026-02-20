15a83f3207a8a1845fc3316bd8b59de09ffb6ef9
WayneHays <waynehays1605@gmail.com>
2026-02-20 14:10:02 +0300

    feat: add MultipartFileDataExtractor

---
+++ src/main/java/com/waynehays/cloudfilestorage/extractor/MultipartFileDataExtractor.java
@@ -0,0 +1,9 @@
+package com.waynehays.cloudfilestorage.extractor;
+
+import com.waynehays.cloudfilestorage.dto.files.FileData;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface MultipartFileDataExtractor {
+
+    FileData handle(MultipartFile file, String directory);
+}
