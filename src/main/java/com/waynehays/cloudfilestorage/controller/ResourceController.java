0553d1abb948f975f0961b55117f84c83fa1ee94
WayneHays <waynehays1605@gmail.com>
2026-02-25 16:21:14 +0300

    feat: add controller for uploading file (ResourceController.uploadFile())

---
+++ src/main/java/com/waynehays/cloudfilestorage/controller/ResourceController.java
@@ -0,0 +1,42 @@
+package com.waynehays.cloudfilestorage.controller;
+
+import com.waynehays.cloudfilestorage.dto.files.response.ResourceDto;
+import com.waynehays.cloudfilestorage.exception.EmptyFileException;
+import com.waynehays.cloudfilestorage.security.CustomUserDetails;
+import com.waynehays.cloudfilestorage.service.file.FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/resource")
+@RequiredArgsConstructor
+public class ResourceController {
+    private final FileService fileService;
+
+    @PostMapping()
+    public ResponseEntity<ResourceDto> uploadFile(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                  @RequestParam(value = "path", required = false) String path,
+                                                  MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new EmptyFileException("File to upload is empty");
+        }
+
+        ResourceDto result = fileService.uploadFile(userDetails.id(), path, file);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<?> downloadFile(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                          @RequestParam("path") String path) {
+
+        return null;
+    }
+}
