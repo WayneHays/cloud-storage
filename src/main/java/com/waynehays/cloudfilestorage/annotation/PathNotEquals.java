fbe2ba3d56eedd6b86808a3ba6aa9c40e10bf114
WayneHays <waynehays1605@gmail.com>
2026-03-16 15:25:22 +0300

    refactor: add annotation for valid path and for paths not equals (for move operation)

---
+++ src/main/java/com/waynehays/cloudfilestorage/annotation/PathNotEquals.java
@@ -0,0 +1,19 @@
+package com.waynehays.cloudfilestorage.annotation;
+
+import com.waynehays.cloudfilestorage.validator.PathNotEqualsValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PathNotEqualsValidator.class)
+public @interface PathNotEquals {
+    String message() default "Source and target paths cannot be equal";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/waynehays/cloudfilestorage/validator/PathNotEqualsValidator.java
@@ -0,0 +1,14 @@
+package com.waynehays.cloudfilestorage.validator;
+
+import com.waynehays.cloudfilestorage.annotation.PathNotEquals;
+import com.waynehays.cloudfilestorage.dto.request.resource.MoveRequest;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PathNotEqualsValidator implements ConstraintValidator<PathNotEquals, MoveRequest> {
+
+    @Override
+    public boolean isValid(MoveRequest request, ConstraintValidatorContext context) {
+        return !request.from().equalsIgnoreCase(request.to());
+    }
+}
