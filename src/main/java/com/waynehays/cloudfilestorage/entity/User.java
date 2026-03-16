64e7b0b8dd23e26ec39b4b94eb0e5b9174ab8f14
WayneHays <waynehays1605@gmail.com>
2026-03-16 16:38:52 +0300

    refactor: remove annotations from User entity, update UserMapper

--- src/main/java/com/waynehays/cloudfilestorage/entity/User.java
+++ src/main/java/com/waynehays/cloudfilestorage/entity/User.java
@@ -6,8 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,8 +15,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class User {
 
     @Id
--- src/main/java/com/waynehays/cloudfilestorage/mapper/UserMapper.java
+++ src/main/java/com/waynehays/cloudfilestorage/mapper/UserMapper.java
@@ -1,11 +1,17 @@
 package com.waynehays.cloudfilestorage.mapper;
 
-import com.waynehays.cloudfilestorage.dto.auth.response.UserDto;
+import com.waynehays.cloudfilestorage.dto.request.auth.SignUpRequest;
+import com.waynehays.cloudfilestorage.dto.response.UserDto;
 import com.waynehays.cloudfilestorage.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     UserDto toDto(User user);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    User toEntity(SignUpRequest signUpRequest);
 }
