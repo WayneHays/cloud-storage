222bbd171d61c0f46618808bfc3f5cebd698cc12
WayneHays <waynehays1605@gmail.com>
2026-03-16 16:44:10 +0300

    feat: add Messages class for common messages

---
+++ src/main/java/com/waynehays/cloudfilestorage/constants/Messages.java
@@ -0,0 +1,9 @@
+package com.waynehays.cloudfilestorage.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class Messages {
+    public static final String ALREADY_EXISTS = "Resource already exists: ";
+    public static final String NOT_FOUND = "Resource not found: ";
+}
