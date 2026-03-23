7a45ac6d5ba54be99c75a57406679d85276fe5e0
WayneHays <waynehays1605@gmail.com>
2026-03-23 11:07:19 +0300

    refactor: UserDetailsServiceImpl -> annotate @NotNull method, extract constant

--- src/main/java/com/waynehays/cloudfilestorage/security/UserDetailsServiceImpl.java
+++ src/main/java/com/waynehays/cloudfilestorage/security/UserDetailsServiceImpl.java
@@ -3,6 +3,7 @@ package com.waynehays.cloudfilestorage.security;
 import com.waynehays.cloudfilestorage.entity.User;
 import com.waynehays.cloudfilestorage.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,12 +12,15 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
+    private static final String MSG_NOT_FOUND = "User with username %s not found";
+
     private final UserRepository userRepository;
 
+    @NotNull
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(@NotNull String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username).orElseThrow(
-                () -> new UsernameNotFoundException("User with username " + username + " not found"));
+                () -> new UsernameNotFoundException(MSG_NOT_FOUND.formatted(username)));
         return new CustomUserDetails(
                 user.getId(),
                 user.getUsername(),
