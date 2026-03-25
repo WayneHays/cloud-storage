55dd8c14147520a98b18a9fe84b5e817627b8eb2
WayneHays <waynehays1605@gmail.com>
2026-03-25 13:07:09 +0300

    refactor: remove constant class Messages

--- src/main/java/com/waynehays/cloudfilestorage/constant/Messages.java
+++
@@ -1,9 +0,0 @@
-package com.waynehays.cloudfilestorage.constant;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public final class Messages {
-    public static final String ALREADY_EXISTS = "Resource already exists: ";
-    public static final String NOT_FOUND = "Resource not found: ";
-}
