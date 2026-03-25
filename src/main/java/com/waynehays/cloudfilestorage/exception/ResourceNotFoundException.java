a5d4f8d4d1ae4b05741ed72a5c8d8dac18260021
WayneHays <waynehays1605@gmail.com>
2026-03-25 13:21:05 +0300

    refactor: remove unused constructor for ResourceNotFoundException

--- src/main/java/com/waynehays/cloudfilestorage/exception/ResourceNotFoundException.java
+++ src/main/java/com/waynehays/cloudfilestorage/exception/ResourceNotFoundException.java
@@ -2,10 +2,6 @@ package com.waynehays.cloudfilestorage.exception;
 
 public class ResourceNotFoundException extends ApplicationException {
 
-    public ResourceNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
     public ResourceNotFoundException(String message) {
         super(message);
     }
