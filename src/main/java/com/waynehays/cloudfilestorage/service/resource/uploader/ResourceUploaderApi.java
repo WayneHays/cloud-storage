be341a98c03927c968e8a636b523da7b92d4edae
WayneHays <waynehays1605@gmail.com>
2026-03-25 13:18:52 +0300

    refactor: rename FileData to ObjectData

--- src/main/java/com/waynehays/cloudfilestorage/service/resource/uploader/ResourceUploaderApi.java
+++ src/main/java/com/waynehays/cloudfilestorage/service/resource/uploader/ResourceUploaderApi.java
@@ -1,11 +1,11 @@
 package com.waynehays.cloudfilestorage.service.resource.uploader;
 
-import com.waynehays.cloudfilestorage.dto.FileData;
+import com.waynehays.cloudfilestorage.dto.ObjectData;
 import com.waynehays.cloudfilestorage.dto.response.ResourceDto;
 
 import java.util.List;
 
 public interface ResourceUploaderApi {
 
-    List<ResourceDto> upload(Long userId, List<FileData> files);
+    List<ResourceDto> upload(Long userId, List<ObjectData> files);
 }
