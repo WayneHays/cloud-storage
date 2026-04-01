1664605d262a2f8e08a253c9e620e73fa1b3d47c
WayneHays <waynehays1605@gmail.com>
2026-04-01 12:12:15 +0300

    refactor: update repositories

--- backend/src/main/java/com/waynehays/cloudfilestorage/repository/ResourceMetadataRepository.java
+++ backend/src/main/java/com/waynehays/cloudfilestorage/repository/ResourceMetadataRepository.java
@@ -16,6 +16,24 @@ import java.util.Set;
 @Repository
 public interface ResourceMetadataRepository extends JpaRepository<ResourceMetadata, Long> {
 
+    Optional<ResourceMetadata> findByUserIdAndPathAndMarkedForDeletionFalse(Long userId, String path);
+
+    List<ResourceMetadata> findByUserIdAndPathStartingWithAndMarkedForDeletionFalse(Long userId, String pathPrefix);
+
+    List<ResourceMetadata> findByUserIdAndParentPathAndMarkedForDeletionFalse(Long userId, String parentPath);
+
+    List<ResourceMetadata> findByUserIdAndNameContainingIgnoreCaseAndMarkedForDeletionFalse(Long userId, String query);
+
+    List<ResourceMetadata> findByMarkedForDeletionTrue();
+
+    @Query("""
+            SELECT r.path FROM ResourceMetadata r
+            WHERE r.userId = :userId
+            AND r.path IN :paths
+            AND r.markedForDeletion = false
+            """)
+    Set<String> findExistingPaths(@Param("userId") Long userId, @Param("paths") Set<String> paths);
+
     @Query("""
             SELECT COALESCE(SUM(r.size), 0)
             FROM ResourceMetadata r
@@ -28,14 +46,6 @@ public interface ResourceMetadataRepository extends JpaRepository<ResourceMetada
                          @Param("type") ResourceType type);
 
     @Query("""
-            SELECT r.path FROM ResourceMetadata r
-            WHERE r.userId = :userId
-            AND r.path IN :paths
-            AND r.markedForDeletion = false
-            """)
-    Set<String> findExistingPaths(@Param("userId") Long userId, @Param("paths") Set<String> paths);
-
-    @Query("""
             SELECT r.userId AS userId, COALESCE(SUM(r.size), 0) AS totalSize
             FROM ResourceMetadata r
             WHERE r.type = :type AND userId IN :userIds
@@ -43,22 +53,6 @@ public interface ResourceMetadataRepository extends JpaRepository<ResourceMetada
             """)
     List<UsedSpace> sumSizeGroupByUserId(@Param("userIds") List<Long> userIds,
                                          @Param("type") ResourceType type);
-
-    Optional<ResourceMetadata> findByUserIdAndPathAndMarkedForDeletionFalse(Long userId, String path);
-
-    List<ResourceMetadata> findByUserIdAndPathStartingWithAndMarkedForDeletionFalse(Long userId, String pathPrefix);
-
-    List<ResourceMetadata> findByUserIdAndParentPathAndMarkedForDeletionFalse(Long userId, String parentPath);
-
-    List<ResourceMetadata> findByUserIdAndNameContainingIgnoreCaseAndMarkedForDeletionFalse(Long userId, String query);
-
-    List<ResourceMetadata> findByMarkedForDeletionTrue();
-
-    boolean existsByUserIdAndPathAndMarkedForDeletionFalse(Long userId, String path);
-
-    void deleteByUserIdAndPath(Long userId, String path);
-
-    void deleteByUserIdAndPathStartingWith(Long userId, String pathPrefix);
 
     @Modifying(clearAutomatically = true)
     @Query("""
@@ -80,7 +74,6 @@ public interface ResourceMetadataRepository extends JpaRepository<ResourceMetada
     void markForDeletionByPrefix(@Param("userId") Long userId,
                                  @Param("prefix") String prefix);
 
-
     @Modifying(clearAutomatically = true)
     @Query("""
             UPDATE ResourceMetadata r
@@ -92,4 +85,8 @@ public interface ResourceMetadataRepository extends JpaRepository<ResourceMetada
     void updatePathsByPrefix(@Param("userId") Long userId,
                              @Param("prefixFrom") String prefixFrom,
                              @Param("prefixTo") String prefixTo);
+
+    void deleteByUserIdAndPath(Long userId, String path);
+
+    void deleteByUserIdAndPathStartingWith(Long userId, String pathPrefix);
 }
--- backend/src/main/java/com/waynehays/cloudfilestorage/repository/UserRepository.java
+++ backend/src/main/java/com/waynehays/cloudfilestorage/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.waynehays.cloudfilestorage.repository;
 
 import com.waynehays.cloudfilestorage.entity.User;
 import jakarta.persistence.LockModeType;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
@@ -28,4 +31,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
             """)
     void updateUsedSpace(@Param("userId") Long userId,
                          @Param("usedSpace") long usedSpace);
+
+    @NotNull
+    Page<User> findAll(@NotNull Pageable pageable);
 }
