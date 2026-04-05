c516a121e68955c8567af6f22cc0f2164f076b9a
waynehays <waynehays1605@gmail.com>
2026-04-05 16:58:48 +0300

    refactor: extract interface for storage service,

---
+++ backend/src/main/java/com/waynehays/cloudfilestorage/service/storage/ResourceStorageServiceApi.java
@@ -0,0 +1,21 @@
+package com.waynehays.cloudfilestorage.service.storage;
+
+import com.waynehays.cloudfilestorage.dto.internal.UploadObjectDto;
+import com.waynehays.cloudfilestorage.resourcestorage.dto.StorageItem;
+
+import java.util.List;
+
+public interface ResourceStorageServiceApi {
+
+    void putObject(Long userId, UploadObjectDto uploadObject);
+
+    StorageItem getObject(Long userId, String path);
+
+    void deleteObject(Long userId, String path);
+
+    void deleteObjects(Long userId, List<String> paths);
+
+    void deleteDirectory(Long userId, String path);
+
+    void moveObject(Long userId, String pathFrom, String pathTo);
+}
