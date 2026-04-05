2da3e5bb1c570af3afd9e7ebb61b133b5c16303b
waynehays <waynehays1605@gmail.com>
2026-04-05 16:48:09 +0300

    fix: add annotation @Mapping to methods

--- backend/src/main/java/com/waynehays/cloudfilestorage/mapper/AuthMapper.java
+++ backend/src/main/java/com/waynehays/cloudfilestorage/mapper/AuthMapper.java
@@ -4,11 +4,15 @@ import com.waynehays.cloudfilestorage.dto.request.auth.SignInRequest;
 import com.waynehays.cloudfilestorage.dto.response.UserDto;
 import com.waynehays.cloudfilestorage.security.CustomUserDetails;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AuthMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "usedSpace", ignore = true)
     UserDto toUserDto(SignInRequest signInRequest);
 
+    @Mapping(target = "usedSpace", ignore = true)
     UserDto toUserDto(CustomUserDetails userDetails);
 }
