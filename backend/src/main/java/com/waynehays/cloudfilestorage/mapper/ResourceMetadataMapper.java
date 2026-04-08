4499617d6b59c4925abc9f536a16d272c3aa568f
waynehays <waynehays1605@gmail.com>
2026-04-08 20:57:25 +0300

    refactor: delete unused methods from mapper

--- backend/src/main/java/com/waynehays/cloudfilestorage/mapper/ResourceMetadataMapper.java
+++ backend/src/main/java/com/waynehays/cloudfilestorage/mapper/ResourceMetadataMapper.java
@@ -1,7 +1,6 @@
 package com.waynehays.cloudfilestorage.mapper;
 
 import com.waynehays.cloudfilestorage.entity.ResourceType;
-import com.waynehays.cloudfilestorage.dto.internal.NewFileDto;
 import com.waynehays.cloudfilestorage.dto.internal.ResourceMetadataDto;
 import com.waynehays.cloudfilestorage.entity.ResourceMetadata;
 import com.waynehays.cloudfilestorage.utils.PathUtils;
@@ -16,30 +15,14 @@ public interface ResourceMetadataMapper {
 
     List<ResourceMetadataDto> toResourceMetadataDto(List<ResourceMetadata> entities);
 
-    default List<ResourceMetadata> toFileEntity(Long userId, List<NewFileDto> newFiles) {
-        return newFiles.stream()
-                .map(f -> toFileEntity(userId, f))
-                .toList();
-    }
-
     default ResourceMetadata toDirectoryEntity(Long userId, String path) {
         String dirPath = PathUtils.ensureTrailingSlash(path);
-        return createEntity(userId, dirPath, ResourceType.DIRECTORY);
-    }
-
-    private ResourceMetadata toFileEntity(Long userId, NewFileDto dto) {
-        ResourceMetadata metadata = createEntity(userId, dto.path(), ResourceType.FILE);
-        metadata.setSize(dto.size());
-        return metadata;
-    }
-
-    private ResourceMetadata createEntity(Long userId, String path, ResourceType type) {
         ResourceMetadata metadata = new ResourceMetadata();
         metadata.setUserId(userId);
-        metadata.setPath(path);
-        metadata.setParentPath(PathUtils.extractParentPath(path));
-        metadata.setName(PathUtils.extractFilename(path));
-        metadata.setType(type);
+        metadata.setPath(dirPath);
+        metadata.setParentPath(PathUtils.extractParentPath(dirPath));
+        metadata.setName(PathUtils.extractFilename(dirPath));
+        metadata.setType(ResourceType.DIRECTORY);
         metadata.setMarkedForDeletion(false);
         return metadata;
     }
