3a547ff8edc2bbf591983636b7e37181c85b7d19
waynehays <waynehays1605@gmail.com>
2026-05-02 14:12:57 +0300

    refactor: final steps

--- backend/src/main/java/com/waynehays/cloudfilestorage/files/api/resource/UploadRequestValidator.java
+++ backend/src/main/java/com/waynehays/cloudfilestorage/files/api/resource/UploadRequestValidator.java
@@ -1,6 +1,5 @@
 package com.waynehays.cloudfilestorage.files.api.resource;
 
-import com.waynehays.cloudfilestorage.core.metadata.exception.ResourceAlreadyExistsException;
 import com.waynehays.cloudfilestorage.files.dto.internal.UploadObjectDto;
 import com.waynehays.cloudfilestorage.infrastructure.path.ResourceLimitsProperties;
 import com.waynehays.cloudfilestorage.infrastructure.path.ValidationUtils;
@@ -41,12 +40,16 @@ class UploadRequestValidator {
                 .toList();
 
         if (!duplicates.isEmpty()) {
-            throw new ResourceAlreadyExistsException("Duplicate paths in upload request", duplicates);
+            throw new UploadValidationException(
+                    duplicates.stream()
+                            .map("'%s': duplicate path in request"::formatted)
+                            .toList()
+            );
         }
     }
 
-    private Optional<String> validate(UploadObjectDto dto) {
-        String filename = dto.filename();
+    private Optional<String> validate(UploadObjectDto object) {
+        String filename = object.filename();
 
         if (ValidationUtils.isInvalidInput(filename)) {
             return Optional.of("'%s': contains invalid characters".formatted(filename));
@@ -55,11 +58,11 @@ class UploadRequestValidator {
             return Optional.of("'%s': exceeds max filename length of %d".formatted(
                     filename, properties.maxFilenameLength()));
         }
-        if (dto.fullPath().length() > properties.maxPathLength()) {
+        if (object.fullPath().length() > properties.maxPathLength()) {
             return Optional.of("'%s': full path exceeds max length of %d".formatted(
                     filename, properties.maxPathLength()));
         }
-        if (dto.size() > properties.maxFileSize().toBytes()) {
+        if (object.size() > properties.maxFileSize().toBytes()) {
             return Optional.of("'%s': exceeds max size of %s".formatted(
                     filename, properties.maxFileSize()));
         }
